a6edbbf2d8f34867211b9dcd0013ad781e6b7967
Fahmi-HI <fahmiiqtishom12@gmail.com>
2026-03-12 11:05:24 +0700

    Implement basic reward rule

---
+++ RewardRule.h
@@ -0,0 +1,16 @@
+#ifndef REWARD_RULE_H
+#define REWARD_RULE_H
+
+class IRewardRule {
+public:
+    virtual int ComputeReward(int baseScore, int round) = 0;
+};
+
+class BasicReward : public IRewardRule {
+public:
+    int ComputeReward(int baseScore, int round) override {
+        return baseScore;
+    }
+};
+
+#endif
