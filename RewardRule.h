996d3568f0cd7a9a35e04e3e1012bb6e6edef87d
Fahmi-HI <fahmiiqtishom12@gmail.com>
2026-03-12 11:20:41 +0700

    Add Modified RewardRule

--- RewardRule.h
+++ RewardRule.h
@@ -13,4 +13,11 @@ public:
     }
 };
 
+class ModifiedReward : public IRewardRule {
+public:
+    int ComputeReward(int baseScore, int round) override {
+        return baseScore +2;
+    }
+};
+
 #endif
